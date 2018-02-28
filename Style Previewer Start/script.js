b4f717ba1ac4dbce723f2d82b40a37d43764df6d
BapeTW <35682140+BapeTW@users.noreply.github.com>
2018-02-28 14:08:10 -0700

    Add files via upload

--- Style Previewer Start/script.js
+++ Style Previewer Start/script.js
@@ -1,1 +1,14 @@
 // STYLE PREVIEWER
+$("#text").change(updateText);
+$("#fontSize").change(updateFontSize);
+
+//EVENT FUNCTION
+function updateText() {
+  let newText = $("#text").val();
+  $("#preview").html(newText);
+}
+
+function updateFontSize() {
+  let newFontSize = Number($("#fontSize").val());
+  $("#preview").css("font-size", newFontSize + "px")
+}
